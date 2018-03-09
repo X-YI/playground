5258d3b8095d8184b4724dfd349d850dd5d97817
YI Tunnel Dev <Dev@yitunnel.com>
2018-03-10 00:19:35 +0800

    generate datasets

--- cubic_equation.py
+++ cubic_equation.py
@@ -4,17 +4,19 @@ import numpy as np
 import random as rd
 
 file = open("data.txt", "a")
+file.write("{},{},{},{},{},{},{},{},{}\n".format('"a"', '"b"', '"c"', '"re1"', '"re2"', '"x11"', '"x12"', '"x21"', '"x22"'))
 
 for i in range(0, 10000):
-    co = [(rd.randint(0, 999999) * power(10, rd.randint(-4, 4))) / 10000 for i in range(3)]
+    co = ([(rd.randint(2, 999999) * power(10, rd.randint(-4, 4))) / 10000 for i in range(3)])
+    co[0] = 1
+    if i % 1000 == 0:
+        print(co)
     try:
         a = co[0]
         b = co[1]
         c = co[2]
         if b * b - 4 * a * c < 0:
             continue
-        if i % 1000 == 0:
-            print(i)
         mp.dps = 7
         # print(co)
         x11 = (-b + sqrt(b * b - 4 * a * c)) / 2 / a
@@ -31,7 +33,7 @@ for i in range(0, 10000):
         re2 = (abs(x21 - x11) / x21)
         # print(re1)
         # print(re2)
-        file.write("{}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(a, b, c, re1, re2, x11, x12, x21, x22))
+        file.write("{},{},{},{},{},{},{},{},{}\n".format(a, b, c, re1, re2, x11, x12, x21, x22))
         
     except Exception as e:
         continue
